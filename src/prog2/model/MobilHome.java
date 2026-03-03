58bcb87cb20172fa9b81474fbb257fe8307989d2
Valero <alex.valero07@gmail.com>
2026-03-03 22:17:30 +0100

    mobilHome corregido

--- src/prog2/model/MobilHome.java
+++ src/prog2/model/MobilHome.java
@@ -2,34 +2,25 @@ package prog2.model;
 
 public class MobilHome extends Casa{
     //Atributs
-    private boolean terrassa, barbacoa;
+    private boolean terrassaBarbacoa;
 
-    public MobilHome(String nom, String idAllotjament,String mida, int numHabitacions, int capacitat, boolean terrassa, boolean barbacoa) {
+    public MobilHome(String nom, String idAllotjament,String mida, int numHabitacions, int capacitat, boolean terrassaBarbacoa) {
         super(nom, idAllotjament, 5, 3, mida, numHabitacions, capacitat);
-        this.terrassa = terrassa;
-        this.barbacoa = barbacoa;
+        this.terrassaBarbacoa = terrassaBarbacoa;
     }
 
-    public boolean getTerrassa(){
-        return terrassa;
+    public boolean getTerrassaBarbacoa(){
+        return terrassaBarbacoa;
     }
 
-    public boolean getBarbacoa(){
-        return barbacoa;
-    }
-
-    public void setTerrassa(boolean terrassa){
-        this.terrassa = terrassa;
-    }
-
-    public void setBarbacoa(boolean barbacoa){
-        this.barbacoa = barbacoa;
+    public void setTerrassaBarbacoa(boolean terrassaBarbacoa){
+        this.terrassaBarbacoa = terrassaBarbacoa;
     }
 
     @Override
     public boolean correcteFuncionament() {
         boolean correcteFuncionament = false;
-        if(terrassa && barbacoa){
+        if(terrassaBarbacoa){
             correcteFuncionament = true;
         }
         return correcteFuncionament;
