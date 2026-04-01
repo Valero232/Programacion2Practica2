9b157f34788326fa41f4a507bc49a77fbe2b9975
aaron <aaronrodriguezraya@gmail.com>
2026-04-02 00:41:11 +0200

    Creación todas las clases + metodos basicos de TascaManteniment

---
+++ src/prog2/model/TascaManteniment.java
@@ -0,0 +1,138 @@
+package prog2.model;
+
+public class TascaManteniment implements InTascaManteniment{
+
+    //Atributs
+    private int numTasca;
+    private Allotjament allotjament;
+    private String data;
+    private TipusTascaManteniment tipus;
+    private int dies;
+
+
+    public TascaManteniment(int numTasca, TipusTascaManteniment tipus, Allotjament allotjament, String data, int dies){
+        this.numTasca = numTasca;
+        this.allotjament = allotjament;
+        this.data = data;
+        this.tipus = tipus;
+        this.dies = dies;
+    }
+
+    /**
+     * Retorna el número identificador de la tasca.
+     *
+     * @return int
+     */
+    @Override
+    public int getNum() {
+        return numTasca;
+    }
+
+    /**
+     * Retorna el tipus de tasca de manteniment.
+     *
+     * @return TipusTascaManteniment
+     */
+    @Override
+    public TascaManteniment.TipusTascaManteniment getTipus() {
+        return tipus;
+    }
+
+    /**
+     * Retorna l'allotjament associat a la tasca.
+     *
+     * @return Allotjament
+     */
+    @Override
+    public Allotjament getAllotjament() {
+        return allotjament;
+    }
+
+    /**
+     * Retorna la data de registre de la tasca.
+     *
+     * @return String
+     */
+    @Override
+    public String getData() {
+        return data;
+    }
+
+    /**
+     * Retorna el nombre de dies previstos per completar la tasca.
+     *
+     * @return int
+     */
+    @Override
+    public int getDies() {
+        return dies;
+    }
+
+    /**
+     * Assigna un nou número identificador a la tasca.
+     *
+     * @param num_ Número identificador de la tasca.
+     */
+    @Override
+    public void setNum(int num_) {
+        numTasca = num_;
+    }
+
+    /**
+     * Assigna el tipus de tasca de manteniment.
+     *
+     * @param tipus_ Tipus de tasca.
+     */
+    @Override
+    public void setTipus(TascaManteniment.TipusTascaManteniment tipus_) {
+        tipus = tipus_;
+    }
+
+    /**
+     * Assigna l'allotjament associat a la tasca.
+     *
+     * @param allotjament_ Allotjament afectat.
+     */
+    @Override
+    public void setAllotjament(Allotjament allotjament_) {
+        allotjament = allotjament_;
+    }
+
+    /**
+     * Assigna la data de registre de la tasca.
+     *
+     * @param data_ Data de la tasca.
+     */
+    @Override
+    public void setData(String data_) {
+        data = data_;
+    }
+
+    /**
+     * Assigna el nombre de dies previstos per completar la tasca.
+     *
+     * @param dies_ Nombre de dies.
+     */
+    @Override
+    public void setDies(int dies_) {
+        dies = dies_;
+    }
+
+    /**
+     * Retorna el percentatge d'il·luminació que ha de tenir l'allotjament
+     * segons el tipus de tasca de manteniment.
+     *
+     * @return String amb el percentatge d'il·luminació.
+     */
+    @Override
+    public String getIluminacioAllotjament() {
+        return "";
+    }
+
+    public static enum TipusTascaManteniment{
+        Reparacio,
+        Neteja,
+        RevisioTecnica,
+        Desinfeccio
+    }
+}
